69718e8f5bbfba31a945bd44fcaff70e1db7b159
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-01-23 08:47:32 -0500

    l3/1 - complete insert-show

---
+++ add-service.php
@@ -0,0 +1,13 @@
+<?php 
+$title = 'Add Service';
+include('shared/header.php'); ?>
+
+<h2>Add a New Service</h2>
+<form method="[something-here]" [something-here]="insert-service.php">
+    <fieldset>
+        <label for="[something-here]">Name: *</label>
+        <input name="name" id="name" required />
+    <button>Submit</button>
+</[something-here]>
+</body>
+</html>
--- insert-show.php
+++ insert-show.php
@@ -12,20 +12,33 @@ $service = $_POST['service'];
 // connect to db using the PDO (PHP Data Objects Library)
 //$db = new PDO('mysql:host=127.0.0.1;dbname=comp1006', 'root', 'x');
 $db = new PDO('mysql:host=127.0.0.1;dbname=comp1006', 'phpdev', 'x');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //$db = new PDO('mysql:host=172.31.22.43;dbname=Rich123456789', 'Rich123456789', 'x');
 
 // set up SQL INSERT command
+// NEVER inject variables directly into SQL; vulnerable to SQL Injection Attacks
+//$sql = "INSERT INTO shows (name, releaseYear, genre, service) VALUES ($name, $releaseYear, $genre, $service)";
+
 $sql = "INSERT INTO shows (name, releaseYear, genre, service) VALUES (:name, :releaseYear, :genre, :service)";
 
+// link db connection w/SQL command we want to run
+$cmd = $db->prepare($sql);
+
 // map each input to a column in the shows table
+$cmd->bindParam(':name', $name, PDO::PARAM_STR, 100);
+$cmd->bindParam(':releaseYear', $releaseYear, PDO::PARAM_INT);
+$cmd->bindParam(':genre', $genre, PDO::PARAM_STR, 20);
+$cmd->bindParam(':service', $service, PDO::PARAM_STR, 100);
 
 // execute the INSERT (which saves to the db)
+$cmd->execute();
 
 // disconnect
+$db = null;
 
 // show msg to user
-
+echo 'Show Saved';
 ?>
 
 </body>
