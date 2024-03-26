10a4c3d10d3c699438a6d73dd978da62c568203e
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-03-26 15:18:28 -0400

    l11/3 - .htaccess fix for 404

--- shared/db.php
+++ shared/db.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', '');
+    $db = new PDO('mysql:host=172.31.22.43;dbname=Rich100', 'Rich100', 'Vda787-KJ_');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
